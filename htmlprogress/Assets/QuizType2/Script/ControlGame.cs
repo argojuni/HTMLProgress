df7c1bc0e0b7e492efafc672f4419c5dcf4d6dee
argojuni <juniargoprasaja05@gmail.com>
2023-12-08 05:10:11 +0700

    Movement Potongan kata ke box

--- htmlprogress/Assets/QuizType2/Script/ControlGame.cs
+++ htmlprogress/Assets/QuizType2/Script/ControlGame.cs
@@ -28,25 +28,103 @@ public class ControlGame : MonoBehaviour
     public int indexPositionHighlight;
     public GameObject[] cloneBoxKatas;
 
-    [Header("Potongan Kata")]
+    [Header("potongan kata")]
     public int[] indexRandomPotonganKata;
     public Text textPotonganKata;
     public int countKata;
     public RectTransform rtPotonganKata;
     public float extraSpacePotonganKata;
 
+    [Header("Move Potongan Kata")]
+    public GameObject potonganKata;
+    public float speedMovementPotonganKata;
+    public bool IsStarMovePotonganKata;
+    Vector3 firstPositionPotonganKata;
+    public bool[] isKataBenar;
+    public GameObject coverBlockMovement;
+
     private void Start()
-    {
+    {      
+
         RandomImageSoal();
 
         GeneratPotonganKata();
+
+        firstPositionPotonganKata = potonganKata.transform.position; //save posisi awal
+
+        isKataBenar = new bool[splitStringImageSoal.Length];//create slot array
+    }
+
+    private void Update()
+    {
+        MovementPotonganKata();
+    }
+
+    public void ButtonPotonganKata()
+    {
+        if(isKataBenar[indexPositionHighlight] == false)
+        {
+            IsStarMovePotonganKata = true;
+
+            coverBlockMovement.SetActive(true);//aktif
+        }
+    }
+
+    void MovementPotonganKata()
+    {
+        if(IsStarMovePotonganKata == true)
+        {
+            potonganKata.transform.position = Vector3.MoveTowards(potonganKata.transform.position, cloneBoxKatas[indexPositionHighlight].transform.position, Time.deltaTime * speedMovementPotonganKata);// move object
+
+            if (Vector3.Distance(potonganKata.transform.position, cloneBoxKatas[indexPositionHighlight].transform.position) < 0.001f)
+            {
+                Debug.Log("Text Sampai Ketujuan");
+                coverBlockMovement.SetActive(false);//mati
+
+                potonganKata.transform.position = firstPositionPotonganKata;//reset position
+
+
+                if (textPotonganKata.text == splitStringImageSoal[indexPositionHighlight])//Index Posisi - count kata respons
+                {
+                    Debug.Log(textPotonganKata.text + " ? " + splitStringImageSoal[indexPositionHighlight]);
+                    Debug.Log("Jawaban Benar");
+
+                    isKataBenar[indexPositionHighlight] = true;//merubah bool atau kondisi box text
+
+                    cloneBoxKatas[indexPositionHighlight].transform.GetChild(0).GetComponent<Text>().text = textPotonganKata.text;//change text dengan jawaban
+
+                    if(countKata < splitStringImageSoal.Length - 1)
+                    {
+                        countKata += 1;//increase karena benar
+
+                        textPotonganKata.text = splitStringImageSoal[indexRandomPotonganKata[countKata]];//update text kata
+
+                        rtPotonganKata.sizeDelta = new Vector2(textPotonganKata.preferredWidth + extraSpacePotonganKata, rtPotonganKata.sizeDelta.y); //resize box
+
+                    }
+                    else
+                    {
+                        Debug.Log("Generate Level Berikutnya");
+                    }
+                }
+                else
+                {
+                    Debug.Log("Jawaban Salah");
+                }
+
+
+                IsStarMovePotonganKata = false;
+            }
+        }
+
+        
     }
 
     void GeneratPotonganKata()
     {
         indexRandomPotonganKata = new int[splitStringImageSoal.Length];//create slot
 
-        for(int i = 0; i<indexRandomPotonganKata.Length; i++)
+        for (int i = 0; i < indexRandomPotonganKata.Length; i++)
         {
             indexRandomPotonganKata[i] = i;//fill array
         }
@@ -60,7 +138,7 @@ public class ControlGame : MonoBehaviour
 
     public void ButtonHighlight()
     {
-        if(EventSystem.current.currentSelectedGameObject.name == "left")
+        if (EventSystem.current.currentSelectedGameObject.name == "left")
         {
             indexPositionHighlight -= 1;
             if (indexPositionHighlight < 0)
@@ -77,9 +155,9 @@ public class ControlGame : MonoBehaviour
             }
         }
 
-        for(int i =0; i<cloneBoxKatas.Length; i++)
+        for (int i = 0; i < cloneBoxKatas.Length; i++)
         {
-            if(i != indexPositionHighlight)
+            if (i != indexPositionHighlight)
             {
                 cloneBoxKatas[i].transform.GetChild(1).gameObject.SetActive(false);//matikan semua
             }
@@ -109,14 +187,14 @@ public class ControlGame : MonoBehaviour
 
         lenghtPerText = new int[splitStringImageSoal.Length];//create slot
 
-        for(int i=0; i<lenghtPerText.Length; i++)
+        for (int i = 0; i < lenghtPerText.Length; i++)
         {
             lenghtPerText[i] = splitStringImageSoal[i].Length;//diisi dari length text
         }
 
-        for(int i = 0; i<lenghtPerText.Length; i++)
+        for (int i = 0; i < lenghtPerText.Length; i++)
         {
-            if(lenghtPerText[i] == Mathf.Max(lenghtPerText))
+            if (lenghtPerText[i] == Mathf.Max(lenghtPerText))
             {
                 indexTextPanjang = i; //index text terpanjang
             }
@@ -125,14 +203,14 @@ public class ControlGame : MonoBehaviour
         cloneBoxKatas = new GameObject[splitStringImageSoal.Length];//create slot array
 
         //respons
-        for(int i=0; i<splitStringImageSoal.Length; i++)
+        for (int i = 0; i < splitStringImageSoal.Length; i++)
         {
             GameObject cloneBoxKata = Instantiate(prefabBoxKata);// respawn
             cloneBoxKata.transform.SetParent(perentKata);// set parent
 
             cloneBoxKatas[i] = cloneBoxKata; //fiill array
 
-            if(i == 0)// for change size x
+            if (i == 0)// for change size x
             {
                 Text textTerpanjang = cloneBoxKata.transform.GetChild(0).GetComponent<Text>();
                 textTerpanjang.text = splitStringImageSoal[indexTextPanjang];
@@ -148,12 +226,12 @@ public class ControlGame : MonoBehaviour
 
             textCloneBoxKata.text = "";
 
-            for (int j=0; j<splitStringImageSoal[i].Length; j++)
+            for (int j = 0; j < splitStringImageSoal[i].Length; j++)
             {
                 textCloneBoxKata.text += "_";
             }
         }
-        
+
 
     }
     void RandomValue(int[] indexRandoms)
